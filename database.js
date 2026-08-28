2829b460a66970c4f1787604ade7203bcbe527fe
Ansel <Ansel.ong.pl@gmail.com>
2026-08-28 17:24:39 +0800

    fixing database pool usage

--- database.js
+++ database.js
@@ -1,24 +1,19 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 
   //host: 'localhost',
   //user: 'root', my SQL username (dun forget)
   //password: 'AXE13a0bAo', This is my SQL pw (dun forget)
   //database: 'myPortfolioDB', This must be Active in Workbench
-});
-
-db.connect(err => {
-  if (err) {
-    console.error('❌ Error connecting ❌:', err);
-    return;
-  }
-  console.log('✅ Connected to MySQL! ✅');
 });
 
 //no db.connect().... pool will manages these connections auto..
